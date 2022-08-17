d328c2bb290b2d9bcf75627bfb57ef6bc82aef3b
Andrew Israel <andrew@propelauth.com>
2022-08-17 16:32:44 -0700

    Add new APIs for migrations

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="propelauth-flask",
-    version="1.1.2",
+    version="1.1.3",
     description="A library for managing authentication in Flask",
     long_description=README,
     long_description_content_type="text/markdown",
