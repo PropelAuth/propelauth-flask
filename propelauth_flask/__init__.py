79d9ab43af1891d3a1c55a047fa6808b47bdeb17
pfvatterott <pfvatterott@gmail.com>
2026-04-17 13:22:50 -0600

    fixes

--- propelauth_flask/__init__.py
+++ propelauth_flask/__init__.py
@@ -607,8 +607,14 @@ class FlaskAuth:
     ):
         return self.auth.fetch_org_scim_groups(org_id, user_id, page_size, page_number)
 
-    def fetch_scim_group(self, org_id: str, group_id: str):
-        return self.auth.fetch_scim_group(org_id, group_id)
+    def fetch_scim_group(
+        self,
+        org_id: str,
+        group_id: str,
+        members_page_size: int = 10,
+        members_page_number: int = 0,
+    ):
+        return self.auth.fetch_scim_group(org_id=org_id, group_id=group_id, members_page_size=members_page_size, members_page_number=members_page_number)
 
     def fetch_user_oauth_tokens(self, user_id: str):
         return self.auth.fetch_user_oauth_tokens(user_id)
@@ -1153,6 +1159,7 @@ class FlaskAuthAsync:
         user_id: Optional[str] = None,
         expires_at_seconds: Optional[str] = None,
         metadata: Optional[Dict[str, Any]] = None,
+        display_name: Optional[str] = None,
     ):
         return await self.auth.import_api_key(
             api_key_token,
@@ -1160,6 +1167,7 @@ class FlaskAuthAsync:
             user_id,
             expires_at_seconds,
             metadata,
+            display_name
         )
 
     async def invite_user_to_org_by_user_id(
@@ -1211,8 +1219,14 @@ class FlaskAuthAsync:
             org_id, user_id, page_size, page_number
         )
 
-    async def fetch_scim_group(self, org_id: str, group_id: str):
-        return await self.auth.fetch_scim_group(org_id, group_id)
+    async def fetch_scim_group(
+        self,
+        org_id: str,
+        group_id: str,
+        members_page_size: int = 10,
+        members_page_number: int = 0,
+    ):
+        return await self.auth.fetch_scim_group(org_id=org_id, group_id=group_id, members_page_size=members_page_size, members_page_number=members_page_number)
 
     async def fetch_user_oauth_tokens(self, user_id: str):
         return await self.auth.fetch_user_oauth_tokens(user_id)
